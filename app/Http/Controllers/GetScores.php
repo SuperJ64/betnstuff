160eade5c1f258b07f9ca8348bb289354e1911e6
Jordan Hayes <hayesjorda@gmail.com>
2015-09-08 15:44:54 -0700

    Added new controller for fetching scores

---
+++ app/Http/Controllers/GetScores.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /*
+    |-----------------------------------------------------------------
+    | Get Scores Controller
+    |-----------------------------------------------------------------
+    |
+    | Fetch the scores for all games and store them in the database.
+    |
+    | @param  string  $url
+    | @return string  $exit_code
+    */
+    public function getScores($id)
+    {
+        print "Hello World";
+    }
+}
+?>
