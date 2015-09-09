614e201464fe59591b787d78269475c7330be9f7
Jordan Hayes <hayesjorda@gmail.com>
2015-09-09 13:48:50 -0700

    Added null handling for scores under Ajax controller

--- app/Http/Controllers/Ajax.php
+++ app/Http/Controllers/Ajax.php
@@ -24,11 +24,32 @@ class Ajax extends Controller
         
         $json_games = array();
         foreach ($games as $game) {
+        	// Get start time
         	$json_games[$game->id]['game']['start'] = $game->start;
-            $json_games[$game->id]['home']['score'] = DB::table('score')->select('home_q1')->where('game_id',$game->id)->value('home_q1');
-            $json_games[$game->id]['home']['team'] = DB::table('team')->select('name','abbr')->where('id',$game->home_team_id)->get();
-            $json_games[$game->id]['away']['score'] = DB::table('score')->select('away_q1')->where('game_id',$game->id)->value('away_q1');
-            $json_games[$game->id]['away']['team'] = DB::table('team')->select('name','abbr')->where('id',$game->away_team_id)->get();
+            
+        	// Get scores and team info for home team
+        	$home_total = DB::table('score')
+        	    ->select('home_q1')
+        	    ->where('game_id',$game->id)
+        	    ->value('home_q1');
+        	if ( !$home_total >= 0){ $json_games[$game->id]['home']['score'] = 'NA'; }
+        	
+            $json_games[$game->id]['home']['team'] = DB::table('team')
+                ->select('name','abbr')
+                ->where('id',$game->home_team_id)
+                ->get();
+            
+            // Get scores and team info for away team
+            $away_total = DB::table('score')
+                ->select('away_q1')
+                ->where('game_id',$game->id)
+                ->value('away_q1');
+            if ( !$away_total >= 0){ $json_games[$game->id]['away']['score'] = 'NA'; }
+            
+            $json_games[$game->id]['away']['team'] = DB::table('team')
+                ->select('name','abbr')
+                ->where('id',$game->away_team_id)
+                ->get();
         }
         
         return response()->json($json_games);
