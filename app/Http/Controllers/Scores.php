809d539e3026b68a3fba9a98c26c45736072f5e6
Jordan Hayes <hayesjorda@gmail.com>
2015-09-09 11:36:43 -0700

    Updated Score controller and uncommented DB queries for full test.

--- app/Http/Controllers/Scores.php
+++ app/Http/Controllers/Scores.php
@@ -39,11 +39,23 @@ class Scores extends Controller
             // Save teams
             if (! $home_team > 0) {
             	print "Inserting home team into DB<br/>";
-                $home_team = DB::table('team')->insertGetId(['abbr' => $game['data']['home']['abbr']]);
+                $home_team = DB::table('team')->insertGetId([
+                		'name' => $game['hnn'],
+                		'abbr' => $game['data']['home']['abbr']
+                ]);
+            } else {
+                print "Updating home team in DB<br/>";
+                DB::table('team')->where('id',$home_team)->update(['name' => $game['hnn']]);
             }
             if (! $away_team > 0) {
-            	print "Inserting away team into DB<br/>";
-                $away_team = DB::table('team')->insertGetId(['abbr' => $game['data']['away']['abbr']]);
+                print "Inserting away team into DB<br/>";
+                $away_team = DB::table('team')->insertGetId([
+                        'name' => $game['vnn'],
+                        'abbr' => $game['data']['away']['abbr']
+                ]);
+            } else {
+                print "Updating away team in DB<br/>";
+                DB::table('team')->where('id',$away_team)->update(['name' => $game['vnn']]);
             }
             
             // Determine start time
@@ -51,21 +63,36 @@ class Scores extends Controller
             $month = substr($eid,4,2);
             $day = substr($eid,6,2);
             $time = explode(':',$game['t']);
-            $hours = $time[0];
+            $hours = $time[0] + 12;
             $minutes = $time[1];
             $seconds = 0;
             $start = date("Y-m-d H:i:s", mktime($hours, $minutes, $seconds, $month, $day, $year));
             
             // Save game
             print "Inserting game for ". $home_team ." ". $away_team ." ". $start ."<br/>";
-            /*DB::table('game')->insert(
+            $gid = DB::table('game')->insertGetId(
                 ['home_team_id' => $home_team,
             	 'away_team_id' => $away_team,
                  'start' => $start]
-            );*/
+            );
             
             // Save scores
-            print "Scores: ". $game['data']['home']['score']['T'] ." ". $game['data']['away']['score']['T']."<br/>";
+            print "Inserting scores ". $game['data']['home']['score']['T']  ." ". $game['data']['away']['score']['T'] ."<br/>";
+            if ($game['data']['home']['score']['T'] != Null && $game['data']['home']['score']['T'] != Null) {
+                DB::table('scrore')->insert([
+                    'game_id'     => $gid,
+                    'home_q1' => $game['data']['home']['score']['1'],
+                    'home_q2' => $game['data']['home']['score']['2'],
+                    'home_q3' => $game['data']['home']['score']['3'],
+                    'home_q4' => $game['data']['home']['score']['4'],
+                    'home_q5' => $game['data']['home']['score']['5'],
+                    'away_q1' => $game['data']['away']['score']['1'],
+                    'away_q2' => $game['data']['away']['score']['2'],
+                    'away_q3' => $game['data']['away']['score']['3'],
+                    'away_q4' => $game['data']['away']['score']['4'],
+                    'away_q5' => $game['data']['away']['score']['5']
+                ]);
+            }
             print "<p>----------------------------------------</p>";
         }
 
