f0b0d60ed81e927c00aabd50cebee81c3670f445
Jordan Hayes <hayesjorda@gmail.com>
2015-09-09 13:32:09 -0700

    Testing Ajax JS for welcome blade

--- app/Http/Controllers/Ajax.php
+++ app/Http/Controllers/Ajax.php
@@ -24,10 +24,11 @@ class Ajax extends Controller
         
         $json_games = array();
         foreach ($games as $game) {
-        	$json_games[$game->id]['game'] = $game;
-            $json_games[$game->id]['score'] = DB::table('score')->where('game_id',$game->id)->get();
-            $json_games[$game->id]['hteam'] = DB::table('team')->where('id',$game->home_team_id)->get();
-            $json_games[$game->id]['vteam'] = DB::table('team')->where('id',$game->away_team_id)->get();
+        	$json_games[$game->id]['game']['start'] = $game->start;
+            $json_games[$game->id]['home']['score'] = DB::table('score')->select('home_q1')->where('game_id',$game->id)->value('home_q1');
+            $json_games[$game->id]['home']['team'] = DB::table('team')->select('name','abbr')->where('id',$game->home_team_id)->value('abbr');
+            $json_games[$game->id]['away']['score'] = DB::table('score')->select('away_q1')->where('game_id',$game->id)->value('away_q1');
+            $json_games[$game->id]['away']['team'] = DB::table('team')->select('name','abbr')->where('id',$game->away_team_id)->value('abbr');
         }
         
         $response = json_encode($json_games);
