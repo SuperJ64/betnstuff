b47c08236b811cf57bdb0d9514a61c9a75473508
Jordan Hayes <hayesjorda@gmail.com>
2015-09-09 12:48:03 -0700

    Added game data to Ajax controller

--- app/Http/Controllers/Ajax.php
+++ app/Http/Controllers/Ajax.php
@@ -24,6 +24,7 @@ class Ajax extends Controller
         
         $json_games = array();
         foreach ($games as $game) {
+        	$json_games[$game->id]['game'] = $game;
             $json_games[$game->id]['score'] = DB::table('score')->where('game_id',$game->id)->get();
             $json_games[$game->id]['hteam'] = DB::table('team')->where('id',$game->home_team_id)->get();
             $json_games[$game->id]['vteam'] = DB::table('team')->where('id',$game->away_team_id)->get();
