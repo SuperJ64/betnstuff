7bbe9c98032cabaa6520654ffaf02356dcf83e36
Jordan Hayes <hayesjordan@gmail.com>
2015-09-12 13:22:15 -0700

    Fixed socres to display 5 quarters...5?

--- app/Http/Controllers/Ajax.php
+++ app/Http/Controllers/Ajax.php
@@ -29,13 +29,18 @@ class Ajax extends Controller
             
         	// Get scores and team info for home team
         	$home_total = DB::table('score')
-        	    //->select('home_q1')
                 ->select(DB::raw('(home_q1 + home_q2 + home_q3 + home_q4 + home_q5) AS t'))
         	    ->where('game_id',$game->id)
         	    ->value('t');
-        	//if ( !$home_total >= 0){ $json_games[$game->id]['home']['score'] = 'NA'; }
-        	$json_games[$game->id]['home']['score'] = $home_total;
         	
+        	// Check home total value
+        	if ($home_total == null) {
+                $json_games[$game->id]['home']['score'] = 'NA';
+        	} else {
+        	    $json_games[$game->id]['home']['score'] = $home_total;
+        	}
+        	
+        	// Get abbr home team name
             $json_games[$game->id]['home']['team']['abbr'] = DB::table('team')
                 ->select('abbr')
                 ->where('id',$game->home_team_id)
@@ -43,11 +48,18 @@ class Ajax extends Controller
             
             // Get scores and team info for away team
             $away_total = DB::table('score')
-                ->select('away_q1')
+                ->select(DB::raw('(away_q1 + away_q2 + away_q3 + away_q4 + away_q5) AS t'))
                 ->where('game_id',$game->id)
-                ->value('away_q1');
-            if ( !$away_total >= 0){ $json_games[$game->id]['away']['score'] = 'NA'; }
+                ->value('t');
             
+            // Check away total value
+            if ($away_total == null) {
+            	$json_games[$game->id]['away']['score'] = 'NA';
+            } else {
+            	$json_games[$game->id]['away']['score'] = $away_total;
+            }
+            
+            // Get abbr away team name
             $json_games[$game->id]['away']['team']['abbr'] = DB::table('team')
                 ->select('abbr')
                 ->where('id',$game->away_team_id)
